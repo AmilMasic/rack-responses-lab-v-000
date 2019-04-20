75425498ee27470d8ed0af5fc3e051cc4821b1a2
Amil Masic <amil.masic@gmail.com>
2019-04-20 11:41:47 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,12 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    resp.write "it is #{Time.now}."
+    time = Time.now
+    if time.hour < 12
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
     resp.finish
   end
 
